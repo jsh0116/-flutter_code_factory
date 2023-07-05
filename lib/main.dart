c7d059147d8f9ee1c69c8d3f85a39f0f8c3189d7
jsh0116 <seunghoon7561@gmail.com>
2023-07-06 00:51:22 +0900

    StatelessWidget 사용해보기
    - StatelessWidget을 상속하는 HomeScreen 선언 및 생성

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp( // MaterialApp 위젯
+    MaterialApp(
+      // MaterialApp 위젯
       debugShowCheckedModeBanner: false,
-      home: Scaffold( // Scaffold 위젯
-        body: SizedBox(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Scaffold 위젯
+      body: SizedBox(
           width: double.infinity,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             // 여러 위젯을 컬럼 위젯에 입력 가능
             children: [
               Text('Code'),
-              Text('Factory'),
+              Text('Factory2'),
             ],
-          )
-        ),
-      ),
-    ),
-  );
+          )),
+    );
+  }
 }
-
