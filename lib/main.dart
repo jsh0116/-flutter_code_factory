7bc152da605e255ce21aff6bca0467109fda31ff
jsh0116 <seunghoon7561@gmail.com>
2023-07-17 00:31:11 +0900

    [refactor] webview 4.x version update
    - webview version 4.x로 update
    - update에 따른 code error 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:code_factory/screen/HomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // flutter 프레임워크가
+  // 앱을 실행할 준비가 될때까지 기다린다.
+  // webview 4.0버전부터 작성 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     MaterialApp( // MaterialApp
       home: HomeScreen(),
--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://blog.codefactory.ai');
+
 class HomeScreen extends StatelessWidget {
 
-  WebViewController? webViewController;
+  WebViewController webViewController = WebViewController()
+  ..setJavaScriptMode(JavaScriptMode.unrestricted)
+  ..loadRequest(homeUrl);
 
   HomeScreen({super.key});
 
@@ -19,22 +23,24 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
             onPressed: () {
-              if (webViewController != null) {
-                webViewController!.loadUrl('https://blog.codefactory.ai');
-              }
+              webViewController.loadRequest(homeUrl);
             },
             icon: Icon(Icons.home),
           )
         ],
       ),
-      body: WebView(
-        // 웹뷰 생성 함수
-        onWebViewCreated: (WebViewController webViewController) {
-          this.webViewController = webViewController;
-        },
-        initialUrl: 'https://blog.codefactory.ai',
-        javascriptMode: JavascriptMode.unrestricted,
-      )
+      body: WebViewWidget(
+        controller: webViewController,
+      ),
+      // webview 3.0 version
+      // WebView(
+      //   // 웹뷰 생성 함수
+      //   onWebViewCreated: (WebViewController webViewController) {
+      //     this.webViewController = webViewController;
+      //   },
+      //   initialUrl: 'https://blog.codefactory.ai',
+      //   javascriptMode: JavascriptMode.unrestricted,
+      // )
     );
   }
 }
